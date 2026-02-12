8dc408688d3dfe28713d1e3458b65d2b73c78085
desarrolloti55 <desarrolloti55_lem@castores.com.mx>
2026-02-12 10:09:48 -0600

    fix : se cambio la forma de obtener el listado de documentos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,61 +1,41 @@
-// 1. Get the query string part of the URL (e.g., "?name=Jonathan&age=30")
-const queryString = window.location.search;
+function obtieneParametroURL_FileName() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get('file');
+}
 
-// 2. Create a URLSearchParams object
-const urlParams = new URLSearchParams(queryString);
+async function muestraArchivoMarkDown(paramFileName) {
 
-// 3. Use the .get() method to retrieve a specific parameter's value
-const file = urlParams.get('file');
+    var filename = 'README.md';
+    if(paramFileName!=null && paramFileName!='' && paramFileName!=filename)
+        filename = 'assets/documents/'+paramFileName;
 
-console.log(file);
-
-filename = 'README.md';
-if(file!=null && file!='' && file!=filename)
-    filename = 'assets/documents/'+file;
-
-console.log(filename);
-
-fetch(filename)
-    .then(response => response.text())
-    .then(markdown => {
-        const converter = new showdown.Converter();
-        const html = converter.makeHtml(markdown);
-        document.getElementById('file_path').innerHTML = filename;
-        document.getElementById('content').innerHTML = html;
-    });
+    fetch(filename)
+        .then(response => response.text())
+        .then(markdown => {
+            const converter = new showdown.Converter();
+            const html = converter.makeHtml(markdown);
+            document.getElementById('file_path').innerHTML = filename;
+            document.getElementById('content').innerHTML = html;
+        }).catch(error => console.error('Error [muestraArchivoMarkDown]:', error));
+}
 
 async function listarArchivos() {
-    const respuesta = await fetch('./assets/documents'); // La carpeta en tu servidor
-    const html = await respuesta.text();
-    
-    console.log(html);
-
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, 'text/html');
-    
-    // Selecciona todos los enlaces (<a>) que suelen ser los nombres de archivo
-    const enlaces = Array.from(doc.querySelectorAll('a'))
-        .map(a => a.innerText)
-        .filter(nombre => !nombre.includes('../')); // Filtrar retrocesos de carpeta
-
-    const filesnames = ['README.md'];
-
-    for (let index = 0; index < enlaces.length; index++) {
-        const element = enlaces[index];
-
-        if(element.includes('.md')){
-            var elementSplit = element.split('.md');
-            var filename = elementSplit[0];
-            filesnames.push(filename + '.md');
-        }       
-    }
-
-    var result = '';
-    filesnames.forEach(function (item) {
-        result += "<li><a href='?file=" + item + "'>" + item + "</a></li>";
-    });
-  
-    document.getElementById('list').innerHTML = result;
-    
+    fetch('./assets/documents/_documents.json')
+        .then(response => response.json())
+        .then(jsonString => {
+            muestraListaArchivos(jsonString.archivos);  
+        }).catch(error => console.error('Error [listarArchivos]:', error));
 }
+
+function muestraListaArchivos(files) {
+    var result = '';
+    files.forEach(function (name) {
+        result += "<li><a href='?file=" + name + "'>" + name + "</a></li>";
+    });  
+    document.getElementById('list').innerHTML = result;    
+}
+
+const paramFileName = obtieneParametroURL_FileName();
+muestraArchivoMarkDown(paramFileName);
 listarArchivos();
